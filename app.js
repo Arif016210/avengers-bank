a9e75140cb15ddf6baf13ca60797af0333ddab40
Arif016210 <arifulislamwasim2017@gmail.com>
2020-09-14 22:55:33 +0600

    login area hide

---
+++ app.js
@@ -0,0 +1,6 @@
+const loginbtn = document.getElementById("login");
+loginbtn.addEventListener("click", function () {
+
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+})
