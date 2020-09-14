7c5c014a7cb35cb418c30184ca6c4424ac48679e
Arif016210 <arifulislamwasim2017@gmail.com>
2020-09-15 00:30:27 +0600

    deposit part compelete

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Login Area Event Handler 
+
 const loginbtn = document.getElementById("login");
 loginbtn.addEventListener("click", function () {
 
@@ -6,4 +8,19 @@ loginbtn.addEventListener("click", function () {
 
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
+});
+
+// Deposit Event Handler
+
+const depositBtn = document.getElementById("deposit-btn");
+depositBtn.addEventListener("click", function () {
+    const depositAmount = document.getElementById("deposit-Amount").value;
+    depositNumber = parseFloat(depositAmount);
+
+    const courrentDeposit = document.getElementById("courrentDeposit").innerText;
+    const courrentDepositNumber = parseFloat(courrentDeposit);
+    const totalDeposit = depositNumber + courrentDepositNumber;
+    document.getElementById("courrentDeposit").innerText = totalDeposit;
+    document.getElementById("deposit-Amount").value = "";
+
 })
