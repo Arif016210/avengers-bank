6695caa248202e8e3d41b6fc0c1b2e27d06a4412
Arif016210 <arifulislamwasim2017@gmail.com>
2020-09-14 23:28:05 +0600

    transaction area add

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ loginbtn.addEventListener("click", function () {
 
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
+
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
 })
